7977e4432414aa23775556597dd9d180f4e53471
Ishan Desai <ishandesai12@gmail.com>
2023-01-26 01:10:18 -0500

    added simple animate on scroll and made other minor changes

---
+++ animate.js
@@ -0,0 +1,14 @@
+//animating the page on scroll
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //mail icon action
+    $('#copy-email').on('click', function() {
+        navigator.clipboard.writeText('ishandesai12@gmail.com')
+    });
+});
