ef6a70b0951b22427338d166df7810bbdb8863f1
Ishan Desai <ishandesai12@gmail.com>
2023-03-14 17:06:22 -0400

    resolved bug of email button not displaying action by adding tooltip message

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     $('#copy-email').on('click', function() {
         navigator.clipboard.writeText('ishandesai12@gmail.com')
     });
+
+    $('[data-toggle="tooltip"]').tooltip({
+        trigger : 'click',
+        container: 'body',
+    })
+    setInterval(function () {
+        $('[data-toggle="tooltip"]').tooltip('hide'); 
+    }, 5000);
 });
